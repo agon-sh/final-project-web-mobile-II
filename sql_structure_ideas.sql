ce2bdc6aff495fc3fdeebfcebf238d70ee15dffa
agon-sh <147383441+agon-sh@users.noreply.github.com>
2025-04-28 14:17:32 +0200

    Update sql_structure_ideas.sql
    added database

--- sql_structure_ideas.sql
+++ sql_structure_ideas.sql
@@ -1,17 +1,20 @@
 --SQL Structure IDEAS:
 -- Data structure for property:
 
+create database empire_living;
+use database empire_living;
+
 create table property (
   PID  int auto_increment Primary key,
   title varchar(30),
   cost  int,
   square_feet  int,
   bedrooms  int,
-  bathrooms  int,
+  bathrooms  int
 );
 
 create table user (
   uername  varchar(50) Primary key,
   email    varchar(100),
   password varchar(100)
-)
+);
