3ca11b12c0a1511d1c8167a6dc1f7acb3aeed6cd
agon-sh <147383441+agon-sh@users.noreply.github.com>
2025-05-05 22:41:03 +0200

    removed css comments

--- login.php
+++ login.php
@@ -12,7 +12,6 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <style>
-        /* Body */
         body {
             font-family: "Poppins", sans-serif;
             background: url('images/bg_signup.jpg') no-repeat center center/cover;
@@ -25,12 +24,10 @@
             align-items: center;
             justify-content: center;
             min-height: calc(100vh - 60px);
-            /* header height */
             padding-top: 60px;
             overflow: hidden;
         }
 
-        /* Container */
         .container {
             background: white;
             padding: 40px 30px;
@@ -47,7 +44,6 @@
             font-family: "Playfair Display", serif;
         }
 
-        /* Form */
         form {
             display: flex;
             flex-direction: column;
@@ -82,7 +78,6 @@
             color: #333;
         }
 
-        /* Buttons */
         input[type="submit"] {
             width: 100%;
             padding: 12px;
